f2141d8adf7c496ae1f576d29b55f5930b4940d8
Ayoamaro <ayoze.amaro@gmail.com>
2020-12-04 19:53:12 +0000

    Update different MODELS

--- src/main/java/dad/javafx/micv/model/Idioma.java
+++ src/main/java/dad/javafx/micv/model/Idioma.java
@@ -1,9 +1,25 @@
 package dad.javafx.micv.model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 /**
  * @author Ayoze Amaro
  *
  */
 public class Idioma {
 
+	private StringProperty certificacion = new SimpleStringProperty();
+
+	public final StringProperty certificacionProperty() {
+		return this.certificacion;
+	}
+	
+	public final String getCertificacion() {
+		return this.certificacionProperty().get();
+	}
+	
+	public final void setCertificacion(final String certificacion) {
+		this.certificacionProperty().set(certificacion);
+	}
 }
